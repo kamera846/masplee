a5c4c021867811a9a6b349b74ec0f69da99e4827
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-04-21 18:18:59 +0700

    feat: hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Hello World</h1>
+    <section class="wrapper bg-gray">
+      <div class="container pt-12 pt-md-14 pb-14 pb-md-16">
+        <div class="row gy-10 gy-md-13 gy-lg-0 align-items-center">
+          <div class="col-md-8 col-lg-5 d-flex position-relative mx-auto" data-cues="slideInDown" data-group="header" data-disabled="true">
+            <div class="img-blob blob-1" data-cue="slideInDown" data-group="header" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 0ms; animation-direction: normal; animation-fill-mode: both;">
+                <img src="/assets/img/photos/me.jpeg" srcset="/assets/img/photos/me.jpeg 2x" alt="">
+            </div>
+          </div>
+          <!--/column -->
+          <div class="col-lg-6 offset-lg-1 col-xxl-5 text-center text-lg-start" data-cues="slideInDown" data-group="page-title" data-delay="600" data-disabled="true">
+            <p class="lead fs-25 lh-sm mb-0 px-md-10 px-lg-0" data-cue="slideInDown" data-group="page-title" data-delay="600" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 900ms; animation-direction: normal; animation-fill-mode: both;"><span class="underline-3 style-2 leaf">Hello! i'm</span></p>
+            <h1 class="display-1 mb-5" data-cue="slideInDown" data-group="page-title" data-delay="600" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 600ms; animation-direction: normal; animation-fill-mode: both;">Mochammad Rafli Ramadani</h1>
+            <p class="lead fs-25 lh-sm mb-7 px-md-10 px-lg-0" data-cue="slideInDown" data-group="page-title" data-delay="600" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 900ms; animation-direction: normal; animation-fill-mode: both;">
+                <span>a Website Developer focused on</span><br> 
+                <span class="typer text-primary underline leaf" 
+                    data-delay="100" 
+                    data-words="Front-End Developer,Slicing Figma Design,API Consumption">
+                </span><span class="cursor text-primary" data-owner="typer"></span>
+            </p>
+            <div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900" data-cue="slideInDown" data-disabled="true" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 900ms; animation-direction: normal; animation-fill-mode: both;">
+              <span data-cue="slideInDown" data-group="page-title-buttons" data-delay="900" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 1200ms; animation-direction: normal; animation-fill-mode: both;"><a href="#" class="btn btn-lg btn-primary rounded-pill me-2">See My Works</a></span>
+              <span data-cue="slideInDown" data-group="page-title-buttons" data-delay="900" data-show="true" style="animation-name: slideInDown; animation-duration: 700ms; animation-timing-function: ease; animation-delay: 1500ms; animation-direction: normal; animation-fill-mode: both;"><a href="#" class="btn btn-lg btn-outline-primary rounded-pill">Contact Me</a></span>
+            </div>
+          </div>
+          <!--/column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
 </template>
