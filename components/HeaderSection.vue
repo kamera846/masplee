d96608a7fc5e2e9779be1c535f450c9a4aa75d00
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-05-08 01:20:52 +0700

    fix: change wording

--- components/HeaderSection.vue
+++ components/HeaderSection.vue
@@ -41,7 +41,7 @@
 								</li>
 								<li class="nav-item">
 									<a class="nav-link" href="/portfolios"
-										>Portfolio</a
+										>Portfolios</a
 									>
 								</li>
 								<li class="nav-item">
